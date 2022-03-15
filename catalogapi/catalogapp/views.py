077b96e996902645d3bd919d7d5cb2970d72a13d
pacha2880 <salguero_ro@hotmail.com>
2022-03-15 04:36:13 -0400

    product model

--- catalogapi/catalogapp/views.py
+++ catalogapi/catalogapp/views.py
@@ -1,3 +1,18 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Home page')
+
+
+def products(request):
+    if request.method == 'GET':
+        try:
+            products = Product.objects.all()
+            response = [product.as_json() for product in products]
+        except:
+            response = 'Error'
+    return HttpResponse(json.dumps(response), content_type='application/json')
